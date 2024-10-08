06a65a800aa14d99517c429bdecd375df8f19c9c
notadev-ui <noteadeveloper@gmail.com>
2024-10-08 14:52:32 +0530

    update

--- Stack/Stack_using_list.py
+++ Stack/Stack_using_list.py
@@ -5,21 +5,24 @@ def push():
     else:
         stack.insert(0,n)
     print(n,"is inserted into stack ")
+    print()
 
 def pop():
     if len(stack) == 0 :
-       print("Stack is empty")
+       print("\nStack is empty.")
     else:
-        print(stack[0],"is deleted from stack")
+        print(stack[0],"is deleted from stack.")
+        print()
         del stack[0] #pop[0]
 
 def display():
     if len(stack) == 0:
         print('Stack is empty')
     else:
-        for i in range:
-            print(i,)
+        for i in stack:
+            print(i)
     print("Top of the stack is", stack[0])
+    print()
 
         
 
@@ -30,7 +33,7 @@ while True:
     print("Enter the option from below\n1-PUSH operation\n2-POP Operatuon\n3-Display\n4-Enter any key to exit")
     option = input()  
     if option == "1":
-        print("Push Operation")
+        print("\nPush Operation")
         push()
     elif option == "2":
         print("POP Operation")
